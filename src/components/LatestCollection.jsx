7165c19db80c2836c8edca69f34a63173b0e9e83
Isha Singh <singhisha366@gmail.com>
2026-06-20 15:50:54 +0530

    Updated LatestCollection and added new BestSeller component

--- src/components/LatestCollection.jsx
+++ src/components/LatestCollection.jsx
@@ -1,10 +1,16 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext';
 import Title from './Title';
+import ProductItem from './ProductItem';
 
 const LatestCollection = () => {
 
   const { products } = useContext(ShopContext);
+  const [latestProducts, setLatestProducts] = useState([]);
+
+  useEffect(() => {
+    setLatestProducts(products.slice(0, 10));
+  }, [])
 
   return (
     <div className='my-10'>
@@ -14,8 +20,20 @@ const LatestCollection = () => {
           Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the.
         </p>
       </div>
+
+      {/* Rendering products */}
+
+      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+        {
+          latestProducts.map((item, index) => (
+            <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price} />
+          ))
+        }
+      </div>
+
     </div>
   )
 }
 
 export default LatestCollection
+
